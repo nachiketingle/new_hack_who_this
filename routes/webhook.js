816a04018d2b3d295a3dad4b7af7e50a7cd1708e
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-07-10 14:09:18 -0700

    fixed webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,23 +1,29 @@
-var express = require("express");
-var app = express();
-var childProcess = require('child_process');
+var express = require('express');
+var router = express.Router();
 
+router.post("/github", function (req, res) {
+	var sender = req.body.sender;
+	var branch = req.body.ref;
 
-app.post("/github", function (req, res) {
-	    var sender = req.body.sender;
-	    var branch = req.body.ref;
-
-	    if(branch.indexOf('Backend') > -1){
-		            deploy(res);
-		        }
+	if(branch.indexOf('Backend') > -1){
+				deploy(res);
+			}
 })
 
 function deploy(res){
-	    childProcess.exec('cd /home/chenaaron3 && ./deploy.sh', function(err, stdout, stderr){
-		            if (err) {
-				             console.error(err);
-				             return res.send(500);
-				            }
-		            res.send(200);
-		          });
+	childProcess.exec('cd /home/chenaaron3 && ./deploy.sh', function(err, stdout, stderr){
+				if (err) {
+						 console.error(err);
+						 return res.send(500);
+						}
+				res.send(200);
+			  });
 }
+
+
+module.exports = router;
+
+
+var express = require("express");
+var app = express();
+var childProcess = require('child_process');
