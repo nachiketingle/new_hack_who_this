7589e278cceaf17c6269dc4ab336a11dc8f681c5
Nachiket Ingle <nachiket.ingle@gmail.com>
2020-07-10 15:13:38 -0700

    Added working timer

--- lib/CustomWidgets/Timer.dart
+++ lib/CustomWidgets/Timer.dart
@@ -1,3 +1,47 @@
-class Timer {
+import 'package:flutter/material.dart';
+import 'dart:async' as async;
+
+class Timer extends StatefulWidget {
+  Timer({@required this.duration, @required this.callback});
+  _TimerState createState() => _TimerState();
+
+  final Function callback;
+  final int duration;
+}
+
+class _TimerState extends State<Timer> {
+
+  async.Timer asyncTimer;
+  int duration;
+
+  @override
+  void initState() {
+    super.initState();
+    duration = widget.duration;
+    asyncTimer = async.Timer.periodic(Duration(seconds: 1), (timer) {
+      duration -= 1;
+      if(mounted) {
+        setState(() {
+
+        });
+      }
+      if(duration <= 0) {
+        timer.cancel();
+        widget.callback.call();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      duration.toString(),
+      textAlign: TextAlign.end,
+      style: TextStyle(
+        fontSize: 50,
+
+      ),
+    );
+  }
 
 }
--- lib/Pages/ChooseWord.dart
+++ lib/Pages/ChooseWord.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:new_hack_who_this/CustomWidgets/Timer.dart';
+import 'package:new_hack_who_this/Helpers/Constants.dart';
 
 class ChooseWord extends StatefulWidget {
 
@@ -7,8 +9,12 @@ class ChooseWord extends StatefulWidget {
 
 class _ChooseWordState extends State<ChooseWord> {
 
-  void _getName() async {
-
+  Future<List<String>> _getName() async {
+      List<String> words = List();
+      words.add('Banana');
+      words.add('Apple');
+      words.add('Orange');
+      return words;
   }
 
   @override
@@ -16,10 +22,40 @@ class _ChooseWordState extends State<ChooseWord> {
     return Scaffold(
       appBar: AppBar(
         title: Text("ChooseWord"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.navigate_next),
+            onPressed: () {
+              Navigator.pushNamed(context, '/previousSketch');
+            },
+          )
+        ],
       ),
       body: Center(
         child: FutureBuilder(
-
+          future: _getName(),
+          builder: (context, snapshot) {
+            if(snapshot.hasData) {
+              List<String> words = snapshot.data;
+              return Stack(
+                children: <Widget>[
+                  Positioned(
+                    right: 20,
+                    top: 10,
+                    child: Timer(
+                      duration: Constants.chooseWordTimer,
+                      callback: () {
+                        Navigator.pushNamed(context, '/previousSketch');
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }
+            else {
+              return CircularProgressIndicator();
+            }
+          },
         )
       ),
     );
