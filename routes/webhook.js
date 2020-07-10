5c01a157aeef425cba7b65e67546fcbcde545290
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-07-10 15:00:24 -0700

    updated webhook

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,29 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var childProcess = require('child_process');
+var scriptDirectory = '/home/chenaaron3'
+var scriptFile = 'deploy.sh'
 
 router.post("/github", function (req, res) {
-	var sender = req.body.sender;
-	var branch = req.body.ref;
+    var sender = req.body.sender;
+    var branch = req.body.ref;
 
-	if(branch.indexOf('Backend') > -1){
-				deploy(res);
-			}
+    if (branch.indexOf('backend') > -1) {
+        res.sendStatus(200);
+        console.log(`Push Detected from ${sender.login}! Now Deploying!`);
+        deploy();
+    }
 })
 
-function deploy(res){
-	childProcess.exec('cd /home/chenaaron3 && ./deploy.sh', function(err, stdout, stderr){
-				if (err) {
-						 console.error(err);
-						 return res.send(500);
-						}
-				res.send(200);
-			  });
+function deploy() {
+    let command = `cd ${scriptDirectory} && ./${scriptFile}`;
+    let child = childProcess.spawn(command, {
+        stdio: 'inherit',
+        shell: true
+    });
+
+    child.on('exit', function (code, signal) {
+        console.log('child process exited with ' + `code ${code} and signal ${signal}`);
+    });
 }
 
-
 module.exports = router;
-
-
-var express = require("express");
-var app = express();
-var childProcess = require('child_process');
