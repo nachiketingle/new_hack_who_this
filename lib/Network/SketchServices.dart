5867c263a037c2744caa344d79ae45dd536bc979
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-07-11 00:34:11 -0700

    previous sketch

--- lib/Network/SketchServices.dart
+++ lib/Network/SketchServices.dart
@@ -1,7 +1,8 @@
 import './Network.dart';
 
 class SketchServices {
-    static Future<Map<String, dynamic>> submitWord(String accessCode, String name, String word) async {
+  static Future<Map<String, dynamic>> submitWord(
+      String accessCode, String name, String word) async {
     Map<String, dynamic> json = Map();
     json['accessCode'] = accessCode;
     json['name'] = name;
@@ -10,5 +11,15 @@ class SketchServices {
     Map<String, dynamic> response = await Network.put("submit-word", json);
     print("Submit Word response: " + response.toString());
     return response;
+  }
+
+  static Future<String> latestSketch(
+      String accessCode, String word) async {
+    Map<String, dynamic> json = Map();
+    json['accessCode'] = accessCode;
+    json['word'] = word;
+    print("Requestion Sketch for " + word);
+    Map<String, dynamic> response = await Network.put("latest-sketch", json);
+    return response["sketch"];
   }
 }
