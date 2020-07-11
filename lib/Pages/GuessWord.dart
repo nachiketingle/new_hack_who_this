9de7d6c3d6e784d21cf79cebf65aa174296a48d5
Nachiket Ingle <nachiket.ingle@gmail.com>
2020-07-10 23:51:24 -0700

    Added basic results page functionality

--- lib/Pages/GuessWord.dart
+++ lib/Pages/GuessWord.dart
@@ -7,6 +7,20 @@ class GuessWord extends StatefulWidget {
 
 class _GuessWordState extends State<GuessWord> {
 
+  int numCorrect = 0;
+
+  /// Get list of words from server
+  Future<List<String>> _getWords() async {
+    List<String> words = List();
+    words.add("Apple");
+    words.add("Banana");
+    words.add("Cucumber");
+
+    await Future.delayed(Duration(seconds: 3));
+    numCorrect = 2;
+    return words;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +28,52 @@ class _GuessWordState extends State<GuessWord> {
         title: Text("GuessWord"),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text("Results Words"),
+        child: FutureBuilder(
+          future: _getWords(),
+          builder: (context, snapshot) {
+            if(snapshot.hasData) {
+              List<String> words = snapshot.data;
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                        height: MediaQuery.of(context).size.height * 0.15,
+                        child: Center(
+                            child: Text("# Correct: " + numCorrect.toString(), style: TextStyle(fontSize: 20),)
+                        )
+                    ),
+                    
+                    Expanded(
+                      child: ListView.builder(
+                        physics: AlwaysScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: words.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: InkWell(
+                              child: Container(
+                                height: MediaQuery.of(context).size.height * 0.2,
+                                child: Center(
+                                  child: Text(words[index], style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                                ),
+                              ),
+                              onTap: () {
+
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+            else {
+              return CircularProgressIndicator();
+            }
+          }
         ),
       ),
     );
